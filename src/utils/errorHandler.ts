a624bf5862811d9648ce01f440c22273cd7e982f
huwf5 <huwf5@mail2.sysu.edu.cn>
2023-09-13 09:25:54 +0800

    fix: change the defaultSetting by using deep copy

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,21 +1,21 @@
 import { ElNotification } from "element-plus";
 
 /**
- * @description 全局代码错误捕捉
+ * @description Global error handler
  * */
 const errorHandler = (error: any) => {
-  // 过滤 HTTP 请求错误
-  if (error.status || error.status == 0) return false;
+  // Filter out HTTP request errors
+  if (error.status || error.status === 0) return false;
   let errorMap: { [key: string]: string } = {
-    InternalError: "Javascript引擎内部错误",
-    ReferenceError: "未找到对象",
-    TypeError: "使用了错误的类型或对象",
-    RangeError: "使用内置对象时，参数超范围",
-    SyntaxError: "语法错误",
-    EvalError: "错误的使用了Eval",
-    URIError: "URI错误"
+    InternalError: "Internal error in the JavaScript engine",
+    ReferenceError: "Object not found",
+    TypeError: "Incorrect type or object used",
+    RangeError: "Parameter out of range when using a built-in object",
+    SyntaxError: "Syntax error",
+    EvalError: "Incorrect use of Eval",
+    URIError: "URI error"
   };
-  let errorName = errorMap[error.name] || "未知错误";
+  let errorName = errorMap[error.name] || "Unknown error";
   ElNotification({
     title: errorName,
     message: error,
