af5e448d7c33729ff2e0506ff26bf29f47921a5d
AdachiAndShimamura <yzq190921@gmail.com>
2024-03-12 17:19:25 +0800

    perf: update build docs
    update build docs for windows platform

--- BuildOnWindows.md
+++ BuildOnWindows.md
@@ -1,27 +1,81 @@
 # 构建指南（Windows）
 
-## 1.环境准备
+## 1.拉取子模块代码
+git submodule update --init --recursive
+
+
+## 2.环境准备
 ### webrtc编译环境(webrtc版本差异会导致所依赖的msvc工具差异):
-(google搜索Windows编译webrtc能找到详细步骤)</br>
-安装visual studio2022，</br>
-MSVC版本: v143, </br>
-Windows11 SDK(10.0.22621.0) </br>
-启用C++ ATL和MFC支持 </br>
-勾选【基于Windows的clang(llvm)工具】
+安装visual studio2022及组件\
+需要的visual studio组件:
+* MSVC: v143
+* Windows11 SDK(10.0.22621.0)
+* 适用于v143生成工具的C++ MFC
+* 适用于v143生成工具的C++ ATL
+* 适用于Windows的C++ Clang工具
 
-
+命令形式： \
+`$ PATH_TO_INSTALLER.EXE
+--add Microsoft.VisualStudio.Workload.NativeDesktop
+--add Microsoft.VisualStudio.Component.VC.ATLMFC
+--add Microsoft.VisualStudio.Component.Windows11SDK.22621
+--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64
+--add Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Llvm.Clang
+--includeRecommended`
 ### msquic编译环境:
-[msquic构建文档](https://github.com/microsoft/msquic/blob/main/docs/BUILD.md) </br>
-根据构建文档安装依赖项(.Net Core,Cmake,Perl)
+[msquic构建文档](https://github.com/microsoft/msquic/blob/main/docs/BUILD.md) \
+根据构建文档安装依赖项(.Net Core,Cmake) \
+也可通过winget包管理器安装依赖项: \
+winget install Microsoft.DotNet.SDK.8 \
+winget install Microsoft.DotNet.DesktopRuntime.8 \
+winget install --id=Kitware.CMake  -e
 
 
-### 本项目编译所需环境:
-配置cl(msvc编译器)工具环境，使其能在powershell中调用</br>
-配置clang(llvm)工具环境，使其能在powershell中调用</br>
-配置INCLUDE和LIB环境，分别包含msvc工具的include/lib目录，和Windows SDK的include/lib目录下的所有子目录
-安装powershell 7
 
+### 本项目编译所需环境(以下示例仅供参考，以实际路径为准):
+* 将 Clang 和 MSVC 路径配置到Path环境变量\
+  MSVC: C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.39.33519\bin\Hostx64\x64 \
+  Clang: C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\Llvm\bin \
+* 配置INCLUDE和LIB环境，分别包含msvc工具的include/lib目录，和Windows SDK的include/lib目录下的所有子目录\
+  INCLUDE: \
+  C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.39.33519\include \
+  C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\ucrt \
+  C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\um \
+  C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\winrt \
+  C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\shared \
+  C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\cppwinrt \
+  LIB: \
+  C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.39.33519\lib\x64 \
+  C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22621.0\ucrt\x64 \
+  C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22621.0\um\x64 \
+  C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22621.0\ucrt_enclave\x64 \
+* 安装powershell 7(Windows powershell存在部分命令无法正确执行的问题，需要新版的powershell 7)
 
-## 编译
-启动powershell 7，在powershell执行根目录下的build.ps1构建脚本</br>
-(如果是首次构建，为了初始化msquic项目，需要在管理员模式执行powershell 7)
+在powershell中配置环境变量(根据自己的实际版本进行修正)：
+
+$clangPath = "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\Llvm\bin" \
+$msvcPath = "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.39.33519\bin\Hostx64\x64"
+
+$env:Path += ";$clangPath;$msvcPath"
+
+$includePaths = @(
+"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.39.33519\include",
+"C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\ucrt",
+"C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\um",
+"C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\winrt",
+"C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\shared",
+"C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\cppwinrt"
+)
+
+$libPaths = @(
+"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.39.33519\lib\x64",
+"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22621.0\ucrt\x64",
+"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22621.0\um\x64",
+"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.22621.0\ucrt_enclave\x64"
+)
+
+$env:INCLUDE = ($includePaths -join ";") \
+$env:LIB = ($libPaths -join ";")
+
+## 3.编译
+在powershell 7中执行根目录下的build.ps1构建脚本
--- build.ps1
+++ build.ps1
@@ -1,10 +1,9 @@
-$WORD_DIR = $PSScriptRoot
-$WEBRTC_DIR="$WORD_DIR/libcs/dep/_google-webrtc"
-$MSQUIC_DIR="$WORD_DIR/libcs/dep/_msquic"
+$WORK_DIR = $PSScriptRoot
+$WEBRTC_DIR="$WORK_DIR/libcs/dep/_google-webrtc"
+$MSQUIC_DIR="$WORK_DIR/libcs/dep/_msquic"
 $WEBRTC_OUT_DIR="$WEBRTC_DIR/src/out/release/obj"
 $MSQUIC_OUT_DIR="$MSQUIC_DIR/build/windows/x64_schannel/obj/Release"
-$MSVC_BUILD_DIR="$WORD_DIR/libcs/msvc-build"
-$RUST_TARGET_DIR="$WORD_DIR/target/x86_64-pc-windows-msvc/release"
+$MSVC_BUILD_DIR="$WORK_DIR/libcs/msvc-build"
 
 $env:CC="clang"
 $env:CXX="clang++"
@@ -12,81 +11,96 @@ $env:CXXFLAGS="-I$WEBRTC_DIR/src -I$WEBRTC_DIR/src/third_party/abseil-cpp -I$MSQ
 $env:CGO_LDFLAGS="-L$MSQUIC_DIR/build/windows/x64_schannel/obj/Release -L$WEBRTC_DIR/src/out/release/obj -lmsquic.lib -lwebrtc.lib"
 $env:CARGO_CFG_TARGET_OS="windows"
 
-Set-Location $WORD_DIR
+$env:DEPOT_TOOLS_WIN_TOOLCHAIN="0"
+$env:GYP_GENERATORS="msvs-ninja,ninja"
+$env:GYP_MSVS_OVERRIDE_PATH="C:\Program Files\Microsoft Visual Studio\2022\Community"
+$env:GYP_MSVS_VERSION="2022"
+
+Set-Location $WORK_DIR
 function complie_webrtc{
     Set-Location "$WEBRTC_DIR/src"
+    Write-Host "开始编译webrtc"
     gn gen out/release --args="clang_use_chrome_plugins=false is_clang=true enable_libaom=false is_component_build=false is_debug=false libyuv_disable_jpeg=true libyuv_include_tests=false rtc_build_examples=false rtc_build_tools=false rtc_enable_grpc=false rtc_enable_protobuf=false rtc_include_builtin_audio_codecs=false rtc_include_dav1d_in_internal_decoder_factory=false rtc_include_ilbc=false rtc_include_internal_audio_device=false rtc_include_tests=false rtc_use_h264=false rtc_use_x11=false treat_warnings_as_errors=false use_custom_libcxx=false use_gold=false use_lld=true use_rtti=true use_sysroot=false"
     ninja -C out/release
-    Set-Location $WORD_DIR
+    if (Test-Path -Path "$WEBRTC_OUT_DIR/webrtc.lib")
+    {
+        Write-Host "webrtc编译完成"
+    }
+    else
+    {
+        Write-Host "webrtc编译失败"
+        Set-Location $WORK_DIR
+        exit
+    }
 }
-if (!(Test-Path -Path "$WEBRTC_OUT_DIR/webrtc.lib")){
-    complie_webrtc
-}
-
 
 function complie_msquic{
     Set-Location $MSQUIC_DIR
-    &./scripts/prepare-machine.ps1
+    Write-Host "msquic开始编译"
     &./scripts/build.ps1 -Config Release -Clean -Static -DisableTest -DisableTools -StaticCRT
-    Set-Location $WORD_DIR
-}
-if (!(Test-Path -Path "$MSQUIC_OUT_DIR/msquic.lib")){
-    if (!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-        Write-Host "请以管理员权限运行此脚本"
+    if (Test-Path -Path "$MSQUIC_OUT_DIR/msquic.lib")
+    {
+        Write-Host "msquic编译完成"
+    }
+    else
+    {
+        Write-Host "msquic编译失败"
+        Set-Location $WORK_DIR
         exit
     }
-    complie_msquic
 }
 
 
 function release_gt_dylib{
-    Set-Location ./libcs
+    Set-Location "$WORK_DIR/libcs"
+    Write-Host "开始编译gt server/client"
     go build -tags release -trimpath -ldflags "-s -w"  -buildmode=c-archive -o release/gt.lib ./lib/export
+    if (Test-Path -Path "./release/gt.lib")
+    {
+        Write-Host "gt server/client编译完成"
+    }
+    else
+    {
+        Write-Host "gt server/client编译失败"
+        Set-Location $WORK_DIR
+        exit
+    }
     Set-Location ./msvc-build
 
-    # 检查target目录是否存在
-    $directory = "$WORD_DIR/libcs/msvc-build/target"
+    $directory = "$WORK_DIR/libcs/msvc-build/target"
     if (-not (Test-Path -Path $directory -PathType Container)) {
         New-Item -Path $directory -ItemType Directory -Force
         Write-Host "目录已创建：$directory"
     } else {
         Write-Host "目录已存在：$directory"
     }
-
-    cl /LD /MT /Fe:./target/gt.dll gt.cpp /link /DEF:gt.def  "../release/gt.lib" "$MSQUIC_OUT_DIR\msquic.lib" "$WEBRTC_OUT_DIR\webrtc.lib" ntdll.lib
-    Set-Location $WORD_DIR
-}
-function release_gt_lib{
-    Set-Location ./libcs
-    go build -tags release -trimpath -ldflags "-s -w"  -buildmode=c-archive -o release/gt.lib ./lib/export
-    Set-Location ./msvc-build
-
-    # 检查target目录是否存在
-    $directory = "$WORD_DIR/libcs/msvc-build/target"
-    if (-not (Test-Path -Path $directory -PathType Container)) {
-        New-Item -Path $directory -ItemType Directory -Force
-        Write-Host "目录已创建：$directory"
-    } else {
-        Write-Host "目录已存在：$directory"
+    Write-Host "开始编译发布gt server/client动态库"
+    cl /LD /MT /Fe:$MSVC_BUILD_DIR/gt.dll gt.cpp /link /DEF:gt.def  "../release/gt.lib" "$MSQUIC_OUT_DIR/msquic.lib" "$WEBRTC_OUT_DIR/webrtc.lib" ntdll.lib
+    if (Test-Path -Path "$MSVC_BUILD_DIR/gt.dll")
+    {
+        Write-Host "动态库编译完成"
     }
-    Set-Location $WORD_DIR
+    else
+    {
+        Write-Host "动态库编译失败"
+        Set-Location $WORK_DIR
+        exit
+    }
 }
-release_gt_dylib
-
 
 function release_gt_exe{
+    Set-Location $WORK_DIR
+    Write-Host "开始编译gt"
     cargo build --target x86_64-pc-windows-msvc -r
+    Write-Host "gt编译完成"
 }
+
+if (!(Test-Path -Path "$WEBRTC_OUT_DIR/webrtc.lib")){
+    complie_webrtc
+}
+
+if (!(Test-Path -Path "$MSQUIC_OUT_DIR/msquic.lib")){
+    complie_msquic
+}
+release_gt_dylib
 release_gt_exe
-
-
-function release_gt_with_dll{
-    # 设置要打包的文件和文件夹路径
-    $filesToCompress = @("$RUST_TARGET_DIR/gt.exe", "$MSVC_BUILD_DIR/target/gt.dll")
-
-    # 设置自解压文件的输出路径和名称
-    $outputFile = "$RUST_TARGET_DIR/gt-manager.exe"
-
-    # 使用 7-Zip 创建自解压文件
-    & 7z a -sfx"D:\Tools\7-Zip\7z.sfx" $outputFile $filesToCompress
-}
